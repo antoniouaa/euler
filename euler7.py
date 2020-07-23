e4f5fa35b0f7931215afa338950ebbe4500c5e9f
antoniouaa <antoniouaa@hotmail.com>
2020-07-23 23:18:00 +0100

    problem 7

---
+++ euler7.py
@@ -0,0 +1,18 @@
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n))+1, 1):
+        if not n % i:
+            return False
+    return True
+
+counter = 0
+num = 2
+while not counter == 10000:
+    num += 1
+    if is_prime(num):
+        counter += 1
+        
+print(num)
